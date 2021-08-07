dd98b79adcdcd0bdc775d01bbd019d5a6e38324e
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-08-07 18:02:30 -0300

    fast_commit at 20210807_180230

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pepino_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
